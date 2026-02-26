d6ca4eefb3917b34966bce4b19b389f372d77935
sandya0 <66195253+sandya0@users.noreply.github.com>
2026-02-26 16:34:14 +0700

    ok

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,6 @@ import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Services from "@/components/Services";
 import Works from "@/components/Works";
-import dynamic from 'next/dynamic';
-
-
-const Services = dynamic(() => import("@/components/Services"));
-const Works = dynamic(() => import("@/components/Works"));
-const Contact = dynamic(() => import("@/components/Contact"));
 
 
 export default function Home() {
